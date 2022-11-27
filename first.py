95a09e5a424fd1a2062bdc5fc2832de9a45d0c50
Antoine <antoine.montier@sfr.fr>
2022-11-27 15:05:55 +0100

    pascal triangle generation is ok

--- first.py
+++ first.py
@@ -1,6 +1,7 @@
 
 
 def printDoubleTable(list):
+    #assert isinstance(list, int[][])
     for i in range(len(list)):
         for j in range(len(list[i])):
             print(list[i][j], end="|")
@@ -8,11 +9,18 @@ def printDoubleTable(list):
 
 def createPascalArray(n):
     assert n > 0 and isinstance(n, int)
-    res = [[]]
-    for i in range(n):
-        res.append([])
-        for j in range(n):
-            res[i].append(j+i)
+    if(n == 1):
+        return [[1]]
+    elif(n==2):
+        return [[1], [1, 1]]
+
+    res = [[1], [1, 1], [1, 2, 1]]
+    for i in range(3, n-1):
+        res.append([1])
+        #start of each line is 1
+        for j in range(1, i):
+            res[i].append(res[i-1][j-1] + res[i-1][j])
+        res[i].append(1)
     return res
 
 
@@ -21,7 +29,7 @@ def createPascalArray(n):
 
 print("\n\n")
 
-pascal = createPascalArray(1)
+pascal = createPascalArray(15)
 
 
 
