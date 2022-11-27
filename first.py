01ae922c9520831e1be9166a46404e85a6f49598
Antoine <antoine.montier@sfr.fr>
2022-11-27 15:30:55 +0100

    fibo, facto, pow and scalar prod added

--- first.py
+++ first.py
@@ -23,18 +23,68 @@ def createPascalArray(n):
         res[i].append(1)
     return res
 
+def fibo(n):
+    assert n >= 0 and isinstance(n, int)
+    if(n == 1):
+        return 1
+    elif(n == 0):
+        return 0
+    else:
+        return fibo(n-1)+fibo(n-2)
+
+def facto(n):
+    assert n >= 0 and isinstance(n, int)
+    if(n <= 1):
+        return 1
+    else:
+        return n * facto(n-1)
+
+def pow(x, n):
+    assert isinstance(n, int) and not(x == 0 and n == 0)
+    if(n == 0):
+        return 1
+    elif(n > 0):
+        res = x
+        while(n>1):
+            res *= x
+            n -= 1
+        return res
+    else:#n < 0
+        res = 1/x
+        while(n < -1):
+            res /= x
+            n += 1
+        return res
+
+def swap(a, b):#useless cause the variables are transmitted by copy
+    c = a
+    a = b
+    b = c
+
+def scalarproduct(l1, l2):
+    assert len(l1) == len(l2)
+    if len(l1) == 1:
+        return l1[0]*l2[0]
+    else:
+        #create new array without first element :
+        l1_bis = []
+        l2_bis = []
+        for i in range(1, len(l1)):
+            l1_bis.append(l1[i])
+            l2_bis.append(l2[i])
+        return l1[0]*l2[0] + scalarproduct(l1_bis, l2_bis)
 
 
-#pascal = [[1, 0, 0, 0], [1, 1, 0, 0], [1, 2, 1, 0], [1, 3, 3, 1]]
+
+
+
+
+
+
 
 print("\n\n")
 
-pascal = createPascalArray(15)
-
-
-
-printDoubleTable(pascal)
-    
+print(scalarproduct([1, 2, 4], [-1, 8, 3]))
 
 print("\n\n")
 
