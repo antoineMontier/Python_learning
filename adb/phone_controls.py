ba11c31c1e0006f848df34908fa7a5eb1ba7cf86
Antoine <antoine.montier@sfr.fr>
2023-01-20 09:51:52 +0100

    openend and typed into an app using adb

--- adb/phone_controls.py
+++ adb/phone_controls.py
@@ -1,19 +1,38 @@
-import pyautogui, time, os
+import pyautogui, time, os, subprocess, cv2, pyperclip
 
-def openADB(ip_adress, port=5555):
-    command = "adb connect " + ip_adress + ":" + str(port)
-    os.system(command)
-    os.system("adb shell")
+def open_adb():
+    ip_adress = str(input("what is the Ip adress or your android device ?"))
+    print("connection")
 
-def closeADB(ip_adress, port=5555):
-    command = "adb disconnect " + ip_adress + ":" + str(port)
-    os.system("exit")
+    result = subprocess.run(['adb', 'connect', ip_adress+':5555'])
 
-openADB("ip_adress")
+    if not result.returncode == 0:
+        print("Command failed with error code: ", result.returncode)
+        exit()
 
-os.system("input swipe 14 15 200 200")
+    print("entering adb shell, type 'exit' to exit")
 
-time.sleep(5)
 
-os.system("exit")
-#closeADB("ip_adress")
+def open_spotify():
+    result = subprocess.run(["adb", "shell", "am", "start", "-a", "android.intent.action.MAIN", "-c", "android.intent.category.LAUNCHER", "-n", "com.spotify.music/.MainActivity"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if(result.stderr.decode() == ""):
+        print("spotify opened successfully")
+    else:
+        print(result.stderr.decode())
+
+def enter_likedSongs():
+    subprocess.run(["adb", "shell", "input", "tap", '530', '2325'])
+    time.sleep(.5)
+    subprocess.run(["adb", "shell", "input", "tap", '530', '440'])
+    time.sleep(1)
+    subprocess.run(["adb", "shell", "input", "text", "Liked"])
+    subprocess.run(["adb", "shell", "input", "keyevent", "62"]) # Space key
+    subprocess.run(["adb", "shell", "input", "text", "songs"])
+
+
+
+#open_adb()
+#open_spotify()
+#time.sleep(5)
+enter_likedSongs()
